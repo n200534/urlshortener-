3f71e8613b87fcbe12fe7f71df9eff57abe881ce
n200534 <amavarapuakshaykumar@gmail.com>
2026-05-18 00:54:16 +0530

    Fixed Lombok configuration for deployment

--- src/main/java/com/example/urlshortner/controller/UrlController.java
+++ src/main/java/com/example/urlshortner/controller/UrlController.java
@@ -10,11 +10,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import org.springframework.beans.factory.annotation.Value;
 
 @Tag(name="URl APIs")
 @RestController
